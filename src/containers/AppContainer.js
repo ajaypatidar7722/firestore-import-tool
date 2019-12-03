0caef3d32feed3f1bad8d65eb704b020ebd0a282
ajaypatidar7722 <ajaypatidar7722@gmail.com>
2019-12-03 12:35:18 +0530

    [Refactor] config sidebar

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -5,27 +5,83 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import withStyles from '@material-ui/core/styles/withStyles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
 import Notification from '../components/NotificationSnackbar';
 
-const styles = () => ({
+const drawerWidth = 240;
+
+const styles = (theme) => ({
   container: {
     minHeight: 'calc(100% - 64px)',
+  },
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerContainer: {
+    overflow: 'auto',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
   },
 });
 
 const AppContainer = ({ children, classes }) => (
   <>
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6">
-          Firebase Tools
-        </Typography>
-      </Toolbar>
-    </AppBar>
-    <Container className={classes.container}>
-      {children}
-    </Container>
-    <Notification />
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" noWrap>
+            Firebase Tools
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <Toolbar />
+        <div className={classes.drawerContainer}>
+          <List>
+            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+        </div>
+      </Drawer>
+      <main className={classes.content}>
+        <Toolbar />
+        <Container className={classes.container}>
+          {children}
+        </Container>
+        <Notification />
+      </main>
+    </div>
+
   </>
 );
 
