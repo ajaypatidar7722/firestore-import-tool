f8befa9edbcfd76436746f34cb4e51287164ce38
ajaypatidar7722 <ajaypatidar7722@gmail.com>
2019-12-03 17:47:41 +0530

    [Refactor] import list route in root routes

--- src/routes.js
+++ src/routes.js
@@ -5,10 +5,12 @@ import {
   Redirect,
 } from 'react-router-dom';
 import Dashboard from './modules/dashboard/dashboard.routes';
+import List from './modules/list/list.routes';
 
 const routes = (
   <Switch>
     <Route path="/dashboard" component={Dashboard} />
+    <Route path="/list" component={List} />
     <Redirect from="*" to="/dashboard" />
   </Switch>
 );
