e441d7609805ac8f0ef6178527d940d95a9d7b50
ajaypatidar7722 <ajaypatidar7722@gmail.com>
2019-12-03 18:18:49 +0530

    [Refactor] add import route in root routes

--- src/routes.js
+++ src/routes.js
@@ -6,11 +6,13 @@ import {
 } from 'react-router-dom';
 import Dashboard from './modules/dashboard/dashboard.routes';
 import List from './modules/list/list.routes';
+import Import from './modules/import/import.routes';
 
 const routes = (
   <Switch>
     <Route path="/dashboard" component={Dashboard} />
     <Route path="/list" component={List} />
+    <Route path="/import" component={Import} />
     <Redirect from="*" to="/dashboard" />
   </Switch>
 );
